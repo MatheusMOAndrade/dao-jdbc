7ed1e524e4dd37044061b1836df6ad126a71ee2b
MatheusMOAndrade <andradematheusmo@gmail.com>
2024-09-15 19:35:25 -0300

    add: departmentDao implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,17 +1,16 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
-    /**
-     * responsible for instantiating the DAO classes
-     * @return SellerDaoJDBC class
-     */
-    public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC(DB.getConnection());
-    }
-   
-
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 }
